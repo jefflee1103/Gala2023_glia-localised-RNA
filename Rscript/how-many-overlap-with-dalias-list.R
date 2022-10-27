76b7cf0e10275b2a905c6219ad80331c0efc7c5c
jefflee1103 <jefflee1103@gmail.com>
2022-10-27 13:13:43 +0100

    20221027 JL 2

--- Rscript/how-many-overlap-with-dalias-list.R
+++ Rscript/how-many-overlap-with-dalias-list.R
@@ -39,7 +39,33 @@ dalia_list <- tibble(
   )
 )
 
-gene_list <- dalia_list %>%
+dalia_list_v2 <- tibble(
+  X1 = c(
+    "alpha-Cat",
+    "arm",
+    "Atpalpha",
+    "CG1648",
+    "CG42342",
+    "Cip4",
+    "cno",
+    "cold",
+    "Flo2",
+    "Gli",
+    "lost",
+    "kst",
+    "l(1)G0320",
+    "Lac",
+    "Nrg",
+    "nrv2",
+    "Nrx-IV",
+    "Pdi",
+    "Sdc",
+    "Vha55",
+    "zip"
+  )
+)
+
+gene_list <- dalia_list_v2 %>%
   left_join(ens99, by = c("X1" = "gene_name")) %>%
   dplyr::select(gene_id, "gene_name" = X1) %>%
   mutate(
@@ -50,11 +76,10 @@ gene_list <- dalia_list %>%
     is_in_1700 =
       if_else(gene_id %in% id_interest$dmel_gene_id, TRUE, FALSE)
   ) %>%
-  left_join(id_interest, by = c("gene_id" = "dmel_gene_id"))
-
-gene_list %>%
+  left_join(id_interest, by = c("gene_id" = "dmel_gene_id")) %>%
   arrange(desc(is_mouse_dmel_convertible), desc(is_in_1700))
 
+View(gene_list)
 
 
 
